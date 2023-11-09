520fc5306bf3cda52e705e9da3e074665c4de3e0
Julia Drozdz <drozdz.julia.witkoria@gmail.com>
2023-11-09 19:07:21 +0000

    base for content of cards

--- Assets/HowToPlay/HowToPlay.cs
+++ Assets/HowToPlay/HowToPlay.cs
@@ -1,11 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit {}
+}
 
 namespace HowToPlay
 {
     public class HowToPlay : MonoBehaviour
     {
+        //  https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
+        private record Card(string Title, string Text);
+
+        private List<Card> _cards = new()
+        {
+            new Card("How to win", "This is how you win the game"),
+            new Card("Swiping tiles", "This is how you swipe the tiles"),
+        };
+        
         private void OnEnable()
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
@@ -21,16 +37,22 @@ namespace HowToPlay
 
             if (nextButton != null)
             {
-                nextButton.clicked += () => Debug.Log("Next");
+                nextButton.clicked += () => Debug.Log("Next"); // todo: implement
             }
 
             if (previousButton != null)
             {
-                previousButton.clicked += () => Debug.Log("Previous");
+                previousButton.clicked += () => Debug.Log("Previous"); // todo: implement
             }
+            
+            var cardTitleLabel = root.Q<Label>("CardTitle");
+            var cardTextLabel  = root.Q<Label>("CardText");
+
+            cardTitleLabel.text = _cards.First().Title;
+            cardTextLabel.text = _cards.First().Text;
         }
         
-        private void GoToMenu()
+        private static void GoToMenu()
         {
             SceneManager.LoadScene("Start/Scene");
         }

2f51063b76f2d1f05c6300133b5216c68f5ce9b1
Julia Drozdz <drozdz.julia.witkoria@gmail.com>
2023-11-14 11:19:58 +0000

    skeleton

---
+++ Assets/Game/Grid/GridSystem.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Game.Grid
+{
+    /// <summary>
+    ///     This class represents the system that manages all tiles.
+    /// </summary>
+    public class GridSystem : MonoBehaviour
+    {
+        private void Spawn()
+        {
+            
+        }
+
+        /// <summary>
+        ///     Listens for an event broadcast produced by a clicked tile and evaluates the matching rules for it.
+        /// </summary>
+        private void EvaluateGrid(Tile tile)
+        {
+            
+        }
+    }
+}
